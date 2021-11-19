67f2eb120d14c59f25fb8fa86b6183660e94829b
jhonticonachambi <jhoticonac@upt.pe>
2021-11-18 20:43:13 -0500

    v2

---
+++ ProyectoFinal.Presentacion/FrmLocalSelect.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProyectoFinal.Negocio;
+
+namespace ProyectoFinal.Presentacion
+{
+    public partial class FrmLocalSelect : Form
+    {
+        public string clocal = "";
+
+        public FrmLocalSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLocalSelect_Load(object sender, EventArgs e)
+        {
+            cmbLocales.Items.Clear();
+            cmbLocales.DataSource = ClsLocalNegocio.Listar();
+            cmbLocales.DisplayMember = "nombre";
+            cmbLocales.ValueMember = "id_local";
+        }
+
+        private void valores()
+        {
+            FrmLogin MDI = new FrmLogin();
+            MDI.local = clocal;
+            MDI.Show();
+            this.Hide();
+        }
+
+        private void cmbLocales_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAcceder_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnCancelar_Click_1(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void btnAcceder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable tablaLocal = new DataTable();
+                tablaLocal = ClsLocalNegocio.Listar();
+                clocal = cmbLocales.GetItemText(cmbLocales.SelectedItem);
+                if (tablaLocal.Rows.Count <= 0)
+                {
+                    MessageBox.Show("El usuario no existe BD", "Acceso al sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    this.valores();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnAcceder_Click_2(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable tablaLocal = new DataTable();
+                tablaLocal = ClsLocalNegocio.Listar();
+                clocal = cmbLocales.GetItemText(cmbLocales.SelectedItem);
+                if (tablaLocal.Rows.Count <= 0)
+                {
+                    MessageBox.Show("El usuario no existe BD", "Acceso al sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    this.valores();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ ProyectoFinal.Presentacion/FrmSeleccionarInventario.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace ProyectoFinal.Presentacion
+{
+    partial class FrmSeleccionarInventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSeleccionarInventario));
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.cmbInvActivos = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(176, 437);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(121, 69);
+            this.btnSalir.TabIndex = 37;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(176, 326);
+            this.btnSiguiente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(121, 69);
+            this.btnSiguiente.TabIndex = 36;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(43, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 31);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Seleccionar el Inventario";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Salida",
+            "Entrada"});
+            this.cmbTipo.Location = new System.Drawing.Point(143, 238);
+            this.cmbTipo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(169, 24);
+            this.cmbTipo.TabIndex = 43;
+            // 
+            // cmbInvActivos
+            // 
+            this.cmbInvActivos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.cmbInvActivos.FormattingEnabled = true;
+            this.cmbInvActivos.Location = new System.Drawing.Point(52, 154);
+            this.cmbInvActivos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbInvActivos.Name = "cmbInvActivos";
+            this.cmbInvActivos.Size = new System.Drawing.Size(388, 24);
+            this.cmbInvActivos.TabIndex = 42;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(139, 199);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 20);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Seleccionar Tipo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(139, 116);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 20);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Inventarios Activos";
+            // 
+            // FrmSeleccionarInventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(459, 554);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.cmbInvActivos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmSeleccionarInventario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmSeleccionarInventario";
+            this.Load += new System.EventHandler(this.FrmSeleccionarInventario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.ComboBox cmbInvActivos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
